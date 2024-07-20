db090e6250196dec46372bfcee3418f3a9f9cc6d
Todd <ihatespam@gmail.com>
2024-07-20 12:02:20 -0500

    Revert "pwa"
    This reverts commit a65311c10450dfd2d1b1ac0afeb47804e767b096.

--- astro.config.ts
+++ astro.config.ts
@@ -9,102 +9,68 @@ import icon from "astro-icon";
 import fs from "fs";
 import rehypeExternalLinks from "rehype-external-links";
 import remarkUnwrapImages from "remark-unwrap-images";
-import { VitePWA } from "vite-plugin-pwa";
 
 import { expressiveCodeOptions } from "./src/site.config";
 import { remarkReadingTime } from "./src/utils/remark-reading-time";
 
 // https://astro.build/config
 export default defineConfig({
-  image: {
-    domains: ["webmention.io"],
-  },
-  integrations: [
-    expressiveCode(expressiveCodeOptions),
-    icon(),
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    sitemap(),
-    mdx(),
-    react(),
-  ],
-  markdown: {
-    rehypePlugins: [
-      [
-        rehypeExternalLinks,
-        {
-          rel: ["nofollow, noopener, noreferrer"],
-          target: "_blank",
-        },
-      ],
-    ],
-    remarkPlugins: [remarkUnwrapImages, remarkReadingTime],
-    remarkRehype: {
-      footnoteLabelProperties: {
-        className: [""],
-      },
-    },
-  },
-  // https://docs.astro.build/en/guides/prefetch/
-  prefetch: true,
-  // ! Please remember to replace the following site property with your own domain
-  site: "https://cuttr.app",
-  vite: {
-    optimizeDeps: {
-      exclude: ["@resvg/resvg-js"],
-    },
-    plugins: [
-      rawFonts([".ttf", ".woff"]),
-      VitePWA({
-        registerType: 'autoUpdate',
-        includeAssets: ['favicon.svg', 'robots.txt', 'apple-touch-icon.png'],
-        manifest: {
-          name: 'My Astro PWA',
-          short_name: 'AstroPWA',
-          description: 'My awesome Astro PWA!',
-          icons: [
-            {
-              src: '/icons/icon-192x192.png',
-              sizes: '192x192',
-              type: 'image/png',
-            },
-            {
-              src: '/icons/icon-512x512.png',
-              sizes: '512x512',
-              type: 'image/png',
-            },
-            {
-              src: '/icons/icon-512x512.png',
-              sizes: '512x512',
-              type: 'image/png',
-              purpose: 'any maskable',
-            },
-          ],
-          start_url: '/',
-          display: 'standalone',
-          theme_color: '#ffffff',
-          background_color: '#ffffff',
-        },
-      }),
-    ],
-  },
+	image: {
+		domains: ["webmention.io"],
+	},
+	integrations: [
+		expressiveCode(expressiveCodeOptions),
+		icon(),
+		tailwind({
+			applyBaseStyles: false,
+		}),
+		sitemap(),
+		mdx(),
+		react(),
+	],
+	markdown: {
+		rehypePlugins: [
+			[
+				rehypeExternalLinks,
+				{
+					rel: ["nofollow, noopener, noreferrer"],
+					target: "_blank",
+				},
+			],
+		],
+		remarkPlugins: [remarkUnwrapImages, remarkReadingTime],
+		remarkRehype: {
+			footnoteLabelProperties: {
+				className: [""],
+			},
+		},
+	},
+	// https://docs.astro.build/en/guides/prefetch/
+	prefetch: true,
+	// ! Please remember to replace the following site property with your own domain
+	site: "https://cuttr.app",
+	vite: {
+		optimizeDeps: {
+			exclude: ["@resvg/resvg-js"],
+		},
+		plugins: [rawFonts([".ttf", ".woff"])],
+	},
 });
 
-function rawFonts(ext) {
-  return {
-    name: "vite-plugin-raw-fonts",
-    // @ts-expect-error:next-line
-    transform(_, id) {
-      // eslint-disable-next-line
-      if (ext.some((e) => id.endsWith(e))) {
-        // eslint-disable-next-line
-        const buffer = fs.readFileSync(id);
-        return {
-          code: `export default ${JSON.stringify(buffer)}`,
-          map: null,
-        };
-      }
-    },
-  };
+function rawFonts(ext: string[]) {
+	return {
+		name: "vite-plugin-raw-fonts",
+		// @ts-expect-error:next-line
+		transform(_, id) {
+			// eslint-disable-next-line
+			if (ext.some((e) => id.endsWith(e))) {
+				// eslint-disable-next-line
+				const buffer = fs.readFileSync(id);
+				return {
+					code: `export default ${JSON.stringify(buffer)}`,
+					map: null,
+				};
+			}
+		},
+	};
 }
